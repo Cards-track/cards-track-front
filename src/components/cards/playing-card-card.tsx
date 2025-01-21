5c75b277b303632e382b8fa54c0b41dab01166b6
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-01-21 11:53:40 +0100

    refactor(card): Refactor card #5

---
+++ src/components/cards/playing-card-card.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { TrendingUp } from "lucide-react";
+import Image from "next/image";
+import { PlayingCardData } from "@/types/playing-card/playing-card-type";
+
+export type PlayingCardCardProps = {
+  card: PlayingCardData;
+};
+
+export function PlayingCardCard({ card }: PlayingCardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{card.name}</CardTitle>
+        <CardDescription>{card.set.name}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex justify-center">
+        <Image
+          src={card.images.small}
+          alt={card.name}
+          width={200}
+          height={280}
+          className="rounded-lg object-contain"
+        />
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              {card.tcgplayer?.prices?.lowestMarket ? (
+                <>
+                  People are willing to pay
+                  <span className="text-accent-foreground text-green-700">
+                    {`$${card.tcgplayer?.prices?.lowestMarket ?? "N/A"}`}
+                  </span>
+                  <TrendingUp className="h-4 w-4 text-green-700" />
+                </>
+              ) : (
+                <>Price informations not available</>
+              )}
+            </div>
+            <div className="flex items-center gap-2 leading-none text-muted-foreground">
+              {card.rarity}
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
